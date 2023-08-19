b7ff1336154e2e93db7bdb8da8e81f85099885ce
nobu1611 <organic.nobu@gmail.com>
2023-08-19 16:07:27 +0900

    修正

--- product/config/routes.rb
+++ product/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # devise_for :users, :controllers => {
-  #   :confirmations => 'users/confirmations',
-  #   :registrations => 'users/registrations',
-  #   :sessions => 'users/sessions',
-  #   :passwords => 'users/passwords'
-  #  }
+  devise_for :users, :controllers => {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+   }
 
   root to: 'recipes#index'
   # get 'recipes/index'
