9cce33fa2697048f9c508376aba1cd49adff88fa
nobu1611 <organic.nobu@gmail.com>
2023-08-19 15:42:23 +0900

    修正

--- product/app/javascript/application.js
+++ product/app/javascript/application.js
@@ -1,16 +1,8 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails";
+import "controllers";
+import * as bootstrap from "bootstrap";
+import "@popperjs/core";
+import "popper";
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-import "@hotwired/turbo-rails"
-import "controllers"
-
-import "@popperjs/core"
-// import "bootstrap"
-// import "bootstrap-icons/font/bootstrap-icons.css"
-// import "stylesheets/application.scss"
-
-// import './custom/recipes';
-import * as bootstrap from "bootstrap"
-import "popper"
-import "bootstrap"
