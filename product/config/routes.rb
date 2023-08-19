084fc50b07d9a77ae8e62d642f2856fe3220fc04
nobu1611 <organic.nobu@gmail.com>
2023-08-19 16:08:32 +0900

    修正

--- product/config/routes.rb
+++ product/config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:index]
+  # resources :users, only: [:index]
   resources :ingredients, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :recipes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :recipe_ingredients
