b882d298786d34066182469be8f3418e6b8de50d
nobu1611 <organic.nobu@gmail.com>
2023-08-19 15:46:06 +0900

    修正

--- product/app/javascript/application.js
+++ product/app/javascript/application.js
@@ -1,8 +1,3 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 import * as bootstrap from "bootstrap";
-import "@popperjs/core";
-import "popper";
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
